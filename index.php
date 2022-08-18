88018436fbad088d96ae2ff65dd24899b0433e1b
Nicolás Romero SJ <nicolas.romero1701@alumnos.ubiobio.cl>
2022-08-17 22:40:36 -0400

    Añadiendo bootstrap

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     <meta charset="utf-8">
     <title>xd</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
